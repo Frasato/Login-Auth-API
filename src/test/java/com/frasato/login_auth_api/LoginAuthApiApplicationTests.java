d3b202f8b788f2b9d44a763c656116ccd26b23fc
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-30 09:37:05 -0300

    Create Project

---
+++ src/test/java/com/frasato/login_auth_api/LoginAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frasato.login_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
